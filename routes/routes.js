d0dd1aad2d7e426c5a6e693f13c636b36ec2e7bd
Gurgen Baghdasaryan <gurgenbaghdasaryan@MacBook-Pro-de-Gurgen.local>
2021-07-27 16:14:36 +0200

    controlers

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,9 @@ const controllers = require('../controllers/controllers')
 router.get('/', controllers.home) 
 router.get('/film/:title', controllers.films) 
 
-//router.post('/film', controllers.filmPost)
+router.post('/film', controllers.filmPost)
+
+router.put('/film' ,controllers.putFilm)
 
 
 
