48a318618c2cdf2322974d05deb8b0d13104f4bf
Gurgen Baghdasaryan <gurgenbaghdasaryan@MacBook-Pro-de-Gurgen.local>
2021-07-27 15:00:49 +0200

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,9 @@ router.get('/', controllers.home)
 router.get('/film/:title', controllers.films) 
 
 //router.post('/film', controllers.filmPost)
+//router.post('/film', controllers.filmPost)
+//router.post('/film', controllers.filmPost)
+//router.post('/film', controllers.filmPost)
 
 
 module.exports = router
